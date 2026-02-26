d6aa4e92f7bd4d718028cf6a649bd0042018129a
Tanupriya91 <tp6677@srmist.edu.in>
2026-02-27 01:05:01 +0530

    changes updated

--- renderer.js
+++ renderer.js
@@ -8,7 +8,7 @@ async function loadTasks() {
 function addTask() {
   const input = document.getElementById("taskInput");
   const priority = document.getElementById("priority").value;
-
+  const dueDate = document.getElementById("dueDate").value;
   if (!input.value) return;
 
   tasks.push({
@@ -16,16 +16,18 @@ function addTask() {
     title: input.value,
     completed: false,
     priority,
+    dueDate,
   });
 
   window.api.saveTasks(tasks);
   input.value = "";
+  document.getElementById("dueDate").value = "";
   renderTasks();
 }
 
 function toggleComplete(id) {
-  tasks = tasks.map(task =>
-    task.id === id ? { ...task, completed: !task.completed } : task
+  tasks = tasks.map((task) =>
+    task.id === id ? { ...task, completed: !task.completed } : task,
   );
 
   window.api.saveTasks(tasks);
@@ -33,25 +35,35 @@ function toggleComplete(id) {
 }
 
 function deleteTask(id) {
-  tasks = tasks.filter(task => task.id !== id);
+  tasks = tasks.filter((task) => task.id !== id);
   window.api.saveTasks(tasks);
   renderTasks();
 }
 
 function renderTasks() {
   const list = document.getElementById("taskList");
+  const today = new Date().toISOString().split("T")[0];
   list.innerHTML = "";
-
-  tasks.forEach(task => {
+  tasks.sort((a, b) => {
+    if (!a.dueDate) return 1;
+    if (!b.dueDate) return -1;
+    return new Date(a.dueDate) - new Date(b.dueDate);
+  });
+  tasks.forEach((task) => {
+    const isOverdue = task.dueDate && task.dueDate < today && !task.completed;
     const li = document.createElement("li");
 
     li.innerHTML = `
-      <span style="text-decoration:${task.completed ? "line-through" : "none"}">
-        ${task.title} (${task.priority})
-      </span>
-      <button onclick="toggleComplete(${task.id})">✔</button>
-      <button onclick="deleteTask(${task.id})">❌</button>
-    `;
+  <span class="task-title ${task.completed ? "completed" : ""} ${isOverdue ? "overdue" : ""}">
+    ${task.title}
+    ${task.dueDate ? ` (Due: ${task.dueDate})` : ""}
+  </span>
+
+  <div>
+    <button onclick="toggleComplete(${task.id})">✅</button>
+    <button onclick="deleteTask(${task.id})">❌</button>
+  </div>
+`;
 
     list.appendChild(li);
   });
